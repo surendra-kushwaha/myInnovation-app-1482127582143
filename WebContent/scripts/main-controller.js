f0ab3b9c42bc23e878160d49f18c7897592e6758
surendra.kushwaha <surendra.kushwaha@accenture.com>
2016-12-27 09:04:08 +0530

    active challenge

--- WebContent/scripts/main-controller.js
+++ WebContent/scripts/main-controller.js
@@ -1,9 +1,19 @@
 app.controller('homeCtrl', function($scope, $location, $state, $http, $rootScope) {
-
+	
+	$scope.backsShow = function () {
+		$('.fronts, .new-rght').hide();
+		$('.backs').show();
+	};
+	
+	$scope.frontsShow = function () {
+		$('.fronts, .new-rght').show();
+		$('.backs').hide();	
+	};
     $scope.openDefault = function () {
       //alert('');
     	//$("#defaultOpen").click();
       $scope.openCity(event, 'London', 1);
+      
       $(".left-arr-menu").off().on("click", function () {
         $(".float-major-menu, .hide-arr-menu").show();
         $(".hide-arr-menu").addClass("midl-menu");
@@ -14,16 +24,32 @@ app.controller('homeCtrl', function($scope, $location, $state, $http, $rootScope
         $(".float-menu").hide();
       });
       $(".ide-det").text(function(index, currentText){return currentText.substr(0, 100);});
-      $(".fnt-msg").off().on("mouseover", function () {
+      $(".fnt-msg").mouseenter(function () {
         $(this).next(".bck-msg").show();
         $(this).hide();
       });
-
-      $(".bck-msg").off().on("mouseout", function () {
+      $(".bck-msg").mouseenter(function () {
+    	  $(this).show();
+      });
+      $('body').not('.bck-msg').mouseenter(function () {
+    	  $(".fnt-msg").show();
+    	  $(".bck-msg").hide();
+      });
+      $(".bck-msg").mouseleave(function () {
         $(this).prev(".fnt-msg").show();
         $(this).hide();
       });
-
+      
+      $("ul.tab li a").off().on("click", function () {
+    	  $(this).addClass('active').parent('li').siblings('li').children('a').removeClass('active');
+    	  if($(this).text() != 'SUB IDEAS' && $(this).text() != 'POPULAR IDEAS'){
+    		  $scope.openCity(event, 'Paris');
+    	  }else{
+    		  $scope.openCity(event, 'London');
+    	  }
+    	  
+      });
+      
       $(".sub-menus1 li").off().on("click", function () {
     	 $(this).addClass('selected');
     	 $(this).siblings('li').removeClass('selected');
@@ -46,9 +72,8 @@ app.controller('homeCtrl', function($scope, $location, $state, $http, $rootScope
 
       $scope.openCity =  function(evt, cityName, flag) {
     // Declare all variables
-
     var i, tabcontent, tablinks;
-
+    //$(this).addClass('active').parent('li').siblings('li').children('a').removeClass('active');
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -58,7 +83,7 @@ app.controller('homeCtrl', function($scope, $location, $state, $http, $rootScope
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+       // tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
     // Show the current tab, and add an "active" class to the link that opened the tab
@@ -69,5 +94,26 @@ app.controller('homeCtrl', function($scope, $location, $state, $http, $rootScope
     }
     //evt.currentTarget.className += " active";
   };
+  
+  $scope.iconRaise = function () {
+	  $(".rolls span img").hover(function () {
+    	  t = $(this).attr('src');
+    	 var tt = t.split(".png");
+    	 console.log(tt[0]+"1.png");
+    	 if(tt[0] != 'images/Details_views-icon'){
+    	 $(this).attr('src', tt[0]+"_Hover.png");
+    	 }
+    	}, function () {
+    		$(this).attr('src', t);
+    	});
+	  
+	  $(".ide-det").each(function () {
+		   var textt = $(this).text().substring(0, 200);
+		   if($(this).text().length>200){
+		 	textt = textt+"...";
+		    }
+		  $(this).text(textt);
+		});
+  }
 });
 
--- WebContent/scripts/main.js
+++ WebContent/scripts/main.js
@@ -8,6 +8,28 @@ app.config(function($stateProvider, $urlRouterProvider) {
             url: '/myinnovative-landing',
             templateUrl: 'views/myinnovative-home.html',
 			controller: 'homeCtrl'
+        })
+        .state('myinnovative-addIdeas', {
+            url: '/myinnovative-addIdeas',
+            templateUrl: 'views/myinnovative-addideas.html',
+			controller: 'homeCtrl'
+        })
+		.state('myinnovative-judge', {
+            url: '/myinnovative-judge',
+            templateUrl: 'views/myinnovative-judge.html',
+			controller: 'homeCtrl'
+        })
+		.state('myinnovative-activechallenge', {
+            url: '/myinnovative-activechallenge',
+            templateUrl: 'views/myinnovative-activechallenge.html',
+			controller: 'homeCtrl'
+        })
+        .state('myinnovative-myIdeas', {
+            url: '/myinnovative-myIdeas',
+            templateUrl: 'views/myinnovative-myideas.html',
+			controller: 'homeCtrl'
         });
 	$urlRouterProvider.otherwise('/myinnovative-landing');
 });
+
+
